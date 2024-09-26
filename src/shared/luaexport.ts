02f9da0b2478e7534300c228012633cc337be4a1
Coolergate <coolergate@gmail.com>
2024-09-26 15:41:18 -0300

    Create lua exports

---
+++ src/shared/luaexport.ts
@@ -0,0 +1,6 @@
+import GameController from "./controllers/prefab/GameController";
+
+export * as entity from "shared/entities";
+export * as userconfig from "shared/userconfig";
+
+export const Controller = GameController;
